c96900578041ca2da967dce62d0b14a9f4a2ac58
George Hickman <george@ghickman.co.uk>
2012-02-10 17:49:28 +0000

    Remove the namespacing of urls

--- discussion/urls.py
+++ discussion/urls.py
@@ -8,10 +8,5 @@ urlpatterns = patterns('discussion.views',
     url(r'^(?P<slug>[\w-]+)/$', DiscussionView.as_view(), name='discussion'),
     url(r'^(?P<discussion_slug>[\w-]+)/posts/$', PostList.as_view()),
     url(r'^(?P<discussion_slug>[\w-]+)/posts/(?P<slug>[\w-]+)/$', PostView.as_view(), name='post'),
-
-    # url(r'^(?P<discussion_slug>[\w-]+)/posts/', include(patterns(
-    #     url(r'^$', PostList.as_view()),
-    #     url(r'^(?P<slug>[\w-]+)/$', PostView.as_view()),
-    # ))
 )
 
