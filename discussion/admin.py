641677e3078635cb6b4a2aac957e2ff1ee7785ea
George Hickman <george@ghickman.co.uk>
2012-02-10 17:49:00 +0000

    Show the admin comments on a post

--- discussion/admin.py
+++ discussion/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from discussion.models import Comment, Discussion, Post
 
 
+class CommentInline(admin.TabularInline):
+    exclude = ('user',)
+    extra = 1
+    model = Comment
+
+
 class PostAdmin(admin.ModelAdmin):
+    inlines = (CommentInline,)
     list_filter = ('discussion',)
+
 
 admin.site.register(Discussion)
 admin.site.register(Post, PostAdmin)
