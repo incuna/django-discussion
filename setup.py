e9227f0b7775a768626a0fe2edd93deb019b5c3c
Charlie Denton <charlie@meshy.co.uk>
2012-06-12 10:45:08 +0100

    Tidied setup file.

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ from setuptools import find_packages, setup
 
 from discussion import get_version
 
+
 def fread(fname):
     with open(join(dirname(__file__), fname), 'r') as f:
         return f.read()
 
 setup(
-    name = "django-discussion",
-    packages = find_packages(),
+    name='django-discussion',
+    packages=find_packages(),
     include_package_data=True,
-    install_requires = [
+    install_requires=[
         'django-orderable>=1.0.1',
         'incuna-notification>=0.3.2',
         ],
-    version = get_version(),
-    description = "",
-    author = "Incuna Ltd",
-    author_email = "dev@incuna.com",
-    url = "http://incuna.com/",
-    long_description=fread("README.md"),
+    version=get_version(),
+    description='',
+    author='Incuna Ltd',
+    author_email='dev@incuna.com',
+    url='http://incuna.com/',
+    long_description=fread('README.md'),
 )
-
